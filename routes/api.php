08ddd5e70285fb8f7fb389dca93e71a1a7df6874
Abel <a.martinez.gpo103pl01@gmail.com>
2020-12-07 11:57:32 -0600

    78

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ use App\Http\Controllers\Api\CandidatoController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource("candidato",CandidatoController::class);
+Route::resource('candidato', CandidatoController::class);
 
